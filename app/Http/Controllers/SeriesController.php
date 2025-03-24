c1f671cc61148979fce7683bd7a5efd72144c89b
Tania Campos <tania.santos36@yahoo.com.br>
2025-03-24 17:19:47 -0300

    fixed date between

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Serie;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class SeriesController extends Controller
 {
@@ -11,12 +12,15 @@ class SeriesController extends Controller
         $series = \DB::table('series')
                         ->where([
                             ['serie', '<>', 'opaa'],
-                            ['createdAt', '>', date('2024-12-31')]
+                            ['created_at', '>', Carbon::parse('2024-12-31')]
                             ])
-                        ->orderBy("serie", "asc")
+                        ->whereBetween('updated_at', [
+                            Carbon::createFromFormat('Y-m-d', '2025-01-01')->startOfDay(),
+                            Carbon::createFromFormat('Y-m-d', '2025-12-31')->endOfday()
+                        ])
+                        ->orderBy("serie", "desc")
                         ->select("serie")
                         ->get();
-
         return view("series.index")->with("series", $series);            
     }
 
